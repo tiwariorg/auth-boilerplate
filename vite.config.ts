89a66efda2c26de5134b112dd4ac1e6b981a8f9d
amantiwari <amantiwari@gofynd.com>
2026-04-22 12:19:52 +0530

    fix(KAN-4): fix CI failures

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
+import { mergeConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['src/test/setup.ts'],
+export default mergeConfig(
+  defineConfig({
+    plugins: [react()],
+  }),
+  {
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: ['src/test/setup.ts'],
+    },
   },
-})
+)
